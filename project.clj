825a02bc3f3d3fca86738033d84f4f8af1a80925
Bobbi Towers <btowers793@gmail.com>
2023-03-27 11:32:22 +0000

    create sound library

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [ring "1.8.2"]
+                 [babashka/fs "0.3.17"]
                  [net.sourceforge.jtransforms/jtransforms "2.4.0"]
                  [com.googlecode.soundlibs/mp3spi "1.9.5-1"]]
   :repl-options {:init-ns ring-app.core}
--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -2,6 +2,7 @@
   (:require [clojure.java.shell :as sh]
             [ring.adapter.jetty :as jetty]
             [ring.util.response :as response]
+            [babashka.fs :as fs]
             [ring.middleware.resource :as resource]
             [clojure.pprint     :as pprint]))
 
@@ -15,8 +16,6 @@
       (* 100 (- n 60))
       (- (* 100 (- 60 n))))))
 
-
-(cents 58)
 
 (defn pitch
  "Takes an integer and filename (optional)
@@ -41,37 +40,35 @@
   (doseq [note (range (- 60 n) (+ 60 n))]
         (pitch note f)))
 
+(def n 60)
+(def f "1")
+
+
 (comment
-  (def n 62)
-  (def f "1")
   (pitch n f)
-  (explode! f 4)
+  (explode! f 16)
+(sh/sh "ls" "resources/audio")
   )
 
+
+(defn audio [f]
+  (str
+  "<div>" f "<audio controls src=\"/" f "\"></audio>"
+  "</div>"))
 
 (defn app [req]
   (case (:uri req)
     "/" {:status 200
          :body "<h1>Homepage</h1>
-                <audio controls src=\"2.wav\"></audio>
                 <ul>
-                    <li><a href=\"/echo\">Echo request</a></li>
-                    <li><a href=\"/greeting\">Greeting</a></li>
+                    <li><a href=\"/sounds\">Sound library</a></li>
                 </ul>"
          :headers {"Content-Type" "text/html; charset=UTF-8"}}
-    "/echo" {:status 200
-             :body (str (with-out-str (pprint/pprint req))
-                        (:out (sh/sh "ls" "resources/audio")))
-             :headers {"Content-Type" "text/plain"}}
-    "/pitch" {:status 200
-              :body 
-              (str "<html><body>" 
-                    (pitch n f)
-                     (str "<audio controls src=\"/" "1-" n ".wav\"></audio></body></html>"))
+    "/sounds" {:status 200
+             :body (str "<html><body>" 
+                        (apply str (map audio (sort (map fs/file-name (fs/glob "resources/audio" "*.wav")))))
+                          "</body></html>")
              :headers {"Content-Type" "text/html; charset=UTF-8"}}
-   "/greeting" {:status 200
-                 :body "<html><body><audio controls src=\"/2.wav\"></audio></body></html>"
-                 :headers {"Content-Type" "text/html; charset=UTF-8"}}
     {:status 404
      :body "Not found."
      :headers {"Content-Type" "text/plain"}}))
